0d9720573a0b7357d8292fc9153243ffddb2c5a4
Hannah Li <hk4li@uwaterloo.ca>
2019-01-19 16:03:41 -0500

    Change webpage to Mastermind

--- src/App.js
+++ src/App.js
@@ -1,27 +1,177 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, TextField } from '@material-ui/core';
 
 class App extends Component {
-  render() {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      errorMsg: '',
+      disableT: true,
+      disableB: false,
+      secret: [
+        1,
+        2,
+        3,
+        4,
+        5
+      ],
+      guess: [
+        1,
+        2,
+        3,
+        4,
+        5
+      ],
+      enteredGuesses: [<p>Guesses: </p>],
+      button: 'Start'
+
+    }
+  }
+
+  getSecret = (secret) => {
+    for (let x = 0; x < 5; x++) {
+      secret[x] = '' + Math.floor(Math.random() * 9 + 1)
+    }
+    console.log('Secret: ' + secret)
+  }
+
+  enterGuess = (enter, guess) => {
+    for (let x = 0; x < 5; x++) {
+      guess[x] = enter.charAt(x);
+    }
+  }
+
+  output = (guess, secret) => {
+    let numString = this.toString(guess) + '   ';
+    
+    let temp = [
+      1,
+      2,
+      3,
+      4,
+      5
+    ]
+    
+    for (let x = 0; x < 5; x++) {
+      temp[x] = guess[x];
+    }
+
+    for (let x = 0; x < 5; x++) {
+      if (temp[x] === secret[x]) {
+        temp[x] = '*';
+      }
+    }
+
+    for (let x = 0; x < 5; x++) {
+      for (let y = 0; y < 5; y++) {
+        if (temp[x] === secret[y] && x !== y) {
+          temp[x] = '+';
+        }
+      }
+    }
+
+    for (let x = 0; x < 5; x++) {
+      if (temp[x] !== '*' && temp[x] !== '+') {
+        temp[x] = '-';
+      }
+    }
+
+    numString += this.toString(temp);
+    console.log(this.state.guess);
+
+    this.state.enteredGuesses.push(<p>{numString}</p>);
+
+    this.setState({
+      enteredGuesses: this.state.enteredGuesses
+    })
+  }
+
+  toString = (list) => {
+    let text = ''
+    for (let x = 0; x < list.length; x++) {
+      text += list[x]
+    }
+    return text
+  }
+
+  check = (guess) => {
+    for (let x = 0; x < 5; x++) {
+      if (guess[x] !== '*') {
+        return false
+      }
+    }
+    return true
+  }
+
+  handleOnChange = (event) => {
+    if (event.target.value.length !== 5) {
+      this.setState({
+        errorMsg: 'Invalid number',
+        disableB: true
+      })
+    } else {
+      this.enterGuess(event.target.value, this.state.guess)
+      this.setState({
+        errorMsg: '',
+        disableB: false,
+        guess: this.state.guess
+      })
+    }
+  }
+
+  handleClick = () => {
+    if (this.state.button === 'Start') {
+      this.getSecret(this.state.secret)
+      this.setState({
+        button: 'Enter',
+        disableT: false,
+        secret: this.state.secret,
+        enteredGuesses: [<p>Guesses: </p>]
+      })
+    } else {
+      this.output(this.state.guess, this.state.secret)
+      if (this.check(this.state.guess)) {
+        this.setState({
+          button: 'Start',
+          disableT: true
+        })
+      }
+    }
+  }
+
+  render () {
+    const textFieldStyle = {
+      width: '250px',
+      margin: '10px 10px'
+    }
+
+    /* title='Enter 5 numbers from 1 to 9 to guess the hidden 5 digit number'
+      hintText='Enter your guess' */
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div stlye={{padding: 20, flexDirection: 'column'}}>
+        <p>The target is to guess a 5 digit number. A "*" means the number is at the right spot,
+          "+" means the number is in the 5 digit number but is in the wrong spot,
+          and "-" means the number is not in the 5 digit number.</p>
+        <div style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}} >
+        <TextField
+          onChange={this.handleOnChange}         
+          disabled={this.state.disableT}
+          style={textFieldStyle}
+          variant="outlined"
+        />
+        <Button
+          variant="outlined"
+          color="primary"
+          disabled={this.state.disableB}
+          onClick={this.handleClick}>
+          {this.state.button}
+        </Button>
+        </div>
+        {this.state.enteredGuesses.map(item => item)}
       </div>
-    );
+    )
   }
 }
 
